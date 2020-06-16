df6afeeb7c207c9b7b14d3ce20d205164b2dc0cb
yuta higa <h.yuta2314@gmail.com>
2020-06-16 20:12:32 +0900

    done! message method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,17 +3,21 @@ class MessagesController < ApplicationController
   before_action :set_group, only: [:index, :create]
 
   def index
-    # @groupsに所属する各ユーザーの名前を配列で返す
-    # @users = User.includes(:group).where(id: params[:group_id])
-    users = @groups.group_users.map {|group| User.find(group.user_id).name }
-    @users = users.join(',')
     @message = Message.new
     @messages = @groups.messages.includes(:user)
+    group_member(@groups)
   end
 
   def create
     @message = @groups.messages.new(params_massage)
     redirect_to group_messages_path(params[:group_id]) if @message.save
+  end
+
+  def group_member(group)
+    # @groupsに所属する各ユーザーの名前を配列で返す
+    # @users = User.includes(:group).where(id: params[:group_id])
+    users = @groups.group_users.map {|group| User.find(group.user_id).name }
+    @users = users.join(',')
   end
 
   private
